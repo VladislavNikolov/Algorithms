7015757716924ef0db0e5f9fd4a8c3483326b225
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2016-04-25 16:04:26 +0300

    Changed

--- Combinatorial-Algorithms/GeneratePermutationsIteratively/GeneratePermutationsIterativelyMain.cs
+++ Combinatorial-Algorithms/GeneratePermutationsIteratively/GeneratePermutationsIterativelyMain.cs
@@ -8,7 +8,6 @@
         {
             Console.Write("Input N:");
             int n = int.Parse(Console.ReadLine());
-            
 
             int key = n - 1;
             int[] currentPermutation = new int[n];
